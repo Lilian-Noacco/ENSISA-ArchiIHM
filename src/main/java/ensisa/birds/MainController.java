ecb907bdc43222acbeb25e93686294d8c8511bc7
lilian.noacco <lilian@noacco.fr>
2025-11-07 11:55:44 +0100

    first class course

---
+++ src/main/java/ensisa/birds/MainController.java
@@ -0,0 +1,40 @@
+package ensisa.birds;
+
+import ensisa.birds.model.Bird;
+import ensisa.birds.model.BirdRepository;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class MainController {
+    private BirdRepository repository;
+
+    @FXML
+    private Label commonNameLabel;
+    @FXML
+    private Label latinNameLabel;
+    @FXML
+    private Label familyLabel;
+    @FXML
+    private Label genusLabel;
+    @FXML
+    private Label specieLabel;
+    @FXML
+    private Label descriptionLabel;
+
+    public Bird currentBird;
+
+    public MainController() {
+        repository = new BirdRepository();
+        repository.load();
+
+        currentBird = repository.birds.get(0);
+    }
+    public void initialize() {
+        commonNameLabel.textProperty().bind(currentBird.commonNameProperty());
+        latinNameLabel.textProperty().bind(currentBird.latinNameProperty());
+        familyLabel.textProperty().bind(currentBird.familyProperty());
+        genusLabel.textProperty().bind(currentBird.genusProperty());
+        specieLabel.textProperty().bind(currentBird.specieProperty());
+        descriptionLabel.textProperty().bind(currentBird.descriptionProperty());
+    }
+}
---
+++ src/main/java/ensisa/birds/model/BirdRepository.java
@@ -0,0 +1,29 @@
+package ensisa.birds.model;
+
+import java.io.InputStream;
+
+import com.fasterxml.jackson.databind.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class BirdRepository {
+    public ObservableList<Bird> birds;
+
+    public BirdRepository() {
+        birds = FXCollections.observableArrayList();
+    }
+
+    public void load() {
+        try (InputStream inputStream = getClass().
+                getResourceAsStream("/ensisa/birds/assets/Birds.json")) {
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.configure(
+                    DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            var birdArray = mapper.readValue(inputStream, Bird[].class);
+            birds.clear();
+            birds.addAll(birdArray);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
